67a41967ef1c4858319036107d8174ad5f6f6807
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-16 18:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-const cube = document.querySelector('.cube');
+const container=document.getElementsByClassName("items")[0];
+const items=document.querySelectorAll(".item");
+items.forEach((item)=>{
+	item.setAttribute("draggable","true")
+	
+})
 
-    let isDragging = false;
-    let startX = 0;
-    let startY = 0;
-    let offsetX = 0;
-    let offsetY = 0;
-
-    // Function to handle the mousedown event
-    function handleMouseDown(event) {
-      isDragging = true;
-      startX = event.clientX;
-      startY = event.clientY;
-      offsetX = parseInt(cube.style.left) || 0;
-      offsetY = parseInt(cube.style.top) || 0;
-    }
-
-    // Function to handle the mousemove event
-    function handleMouseMove(event) {
-      if (!isDragging) return;
-
-      const newX = offsetX + event.clientX - startX;
-      const newY = offsetY + event.clientY - startY;
-
-      cube.style.left = newX + 'px';
-      cube.style.top = newY + 'px';
-    }
-
-    // Function to handle the mouseup event
-    function handleMouseUp() {
-      isDragging = false;
-    }
-
-    // Add event listeners
-    cube.addEventListener('mousedown', handleMouseDown);
-    cube.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
+items.forEach((item)=>{
+	item.addEventListener("dragover",(e)=>{
+		e.preventDefault();
+		let target=e.target;
+		
+	})
+})
