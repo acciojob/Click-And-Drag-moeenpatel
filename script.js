9dbc973e5635dfe6f3f48a3d5e28f552469793d1
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-17 12:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-const slider = document.querySelector(".items");
-let isDown = false;
-let startX;
-let scrollLeft;
 
-// when mouse down active class added so mouse changed to grab and background transforms to full size
-// startX = start position of cursor calculated from event.x position - margin.
-slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
+    const items = document.querySelector('.items');
+    let isDragging = false;
+    let startX = 0;
+    let scrollLeft = 0;
 
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
+    items.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startX = e.pageX - items.offsetLeft;
+      scrollLeft = items.scrollLeft;
 
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
+      items.style.cursor = 'grabbing';
+    });
 
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return; //stop the function from running
-  //console.log(isDown);
-  e.preventDefault(); // prevent selection of text etc inside area.
-  const x = e.pageX - slider.offsetLeft;
-  const scrollMultiple = 3; // scroll 3 pixels for each pixel moved by the mouse.
-  const walk = (x - startX) * scrollMultiple;
-  slider.scrollLeft = scrollLeft - walk;
-  //console.log("slider scrollleft", slider.scrollLeft); //max value 4207.2001953125
-});
+    items.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      e.preventDefault();
+
+      const x = e.pageX - items.offsetLeft;
+      const walk = (x - startX) * 3; // Adjust the sliding speed
+      items.scrollLeft = scrollLeft - walk;
+    });
+
+    items.addEventListener('mouseup', () => {
+      isDragging = false;
+      items.style.cursor = 'grab';
+    });
+
+    items.addEventListener('mouseleave', () => {
+      isDragging = false;
+      items.style.cursor = 'grab';
+    });
