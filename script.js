fc1a126a04c2286e2fc67b8e7ab0ee9b70a1a198
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-17 12:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const container=document.getElementsByClassName("items")[0];
-const items=document.querySelectorAll(".item");
-items.forEach((item)=>{
-	item.setAttribute("draggable","true")
-	
-})
-
-items.forEach((item)=>{
-	item.addEventListener("dragover",(e)=>{
-		e.preventDefault();
-		let target=e.target;
-		
-	})
-})
+ const slider = document.querySelector('.items'); 
+ let isDown = false; 
+ let startX; 
+ let scrollLeft; 
+  
+ slider.addEventListener('mousedown', (e) => { 
+ isDown = true; 
+ slider.classList.add('active'); 
+ startX = e.pageX - slider.offsetLeft; 
+ scrollLeft = slider.scrollLeft; 
+ }); 
+  
+ slider.addEventListener('mouseleave', () => { 
+ isDown = false; 
+ slider.classList.remove('active'); 
+ }); 
+  
+ slider.addEventListener('mouseup', () => { 
+ isDown = false; 
+ slider.classList.remove('active'); 
+ }); 
+  
+ slider.addEventListener('mousemove', (e) => { 
+ if (!isDown) return; // stop the fn from running 
+ e.preventDefault(); 
+ const x = e.pageX - slider.offsetLeft; 
+ const walk = (x - startX) * 3; 
+ slider.scrollLeft = scrollLeft - walk; 
+ });
